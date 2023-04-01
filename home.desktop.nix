22d0676fd49fc6dc800a6e39fc8a7b871b64c468
Tommy Li <dev@stdx.space>
2023-04-01 15:46:41 +0800

    update gnome workspace settings

--- home.desktop.nix
+++ home.desktop.nix
@@ -29,16 +29,30 @@ with lib.hm.gvariant;
       move-to-workspace-2 = ["<Shift><Super>2"];
       move-to-workspace-3 = ["<Shift><Super>3"];
       move-to-workspace-4 = ["<Shift><Super>4"];
+      move-to-workspace-5 = ["<Shift><Super>5"];
+      move-to-workspace-6 = ["<Shift><Super>6"];
+      move-to-workspace-7 = ["<Shift><Super>7"];
+      move-to-workspace-8 = ["<Shift><Super>8"];
+      move-to-workspace-9 = ["<Shift><Super>9"];
+      move-to-workspace-10 = ["<Shift><Super>0"];
       switch-to-workspace-1 = ["<Super>1"]; 
       switch-to-workspace-2 = ["<Super>2"]; 
       switch-to-workspace-3 = ["<Super>3"]; 
       switch-to-workspace-4 = ["<Super>4"]; 
+      switch-to-workspace-5 = ["<Super>5"]; 
+      switch-to-workspace-6 = ["<Super>6"]; 
+      switch-to-workspace-7 = ["<Super>7"]; 
+      switch-to-workspace-8 = ["<Super>8"]; 
+      switch-to-workspace-9 = ["<Super>9"]; 
+      switch-to-workspace-10 = ["<Super>0"]; 
     };
     "org/gnome/desktop/wm/preferences" = {
       button-layout = "close,minimize:appmenu";
+      num-workspaces = 6;
     };
     "org/gnome/mutter" = {
       check-alive-timeout = mkUint32 30000;
+      dynamic-workspaces = false;
     };
     "org/gnome/mutter/keybindings" = {
       toggle-tiled-left = [];
@@ -89,6 +103,18 @@ with lib.hm.gvariant;
     "org/gnome/shell/extensions/pop-shell" = {
       smart-gaps = true;
       tile-by-default = true;
+    };
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
+      switch-to-application-9 = [];
+      switch-to-application-10 = [];
     };
     "org/gnome/shell/weather" = {
       automatic-location = true;
