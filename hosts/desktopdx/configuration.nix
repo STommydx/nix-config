45a03606eb6521e4786b4db951984b998b476ab9
Tommy Li <dev@stdx.space>
2023-09-17 23:41:00 +0800

    create home directory backup

--- hosts/desktopdx/configuration.nix
+++ hosts/desktopdx/configuration.nix
@@ -44,6 +44,25 @@
         Persistent = true;
       };
     };
+    home = {
+      environmentFile = config.sops.secrets.minio_credentials.path;
+      passwordFile = config.sops.secrets.restic_password.path;
+      paths = [
+        "/home/stommydx/Documents"
+        "/home/stommydx/Downloads"
+        "/home/stommydx/Pictures"
+        "/home/stommydx/Videos"
+        "/home/stommydx/workspace"
+      ];
+      pruneOpts = [
+        "--keep-within 30d"
+      ];
+      repository = "s3:https://artifacts.stdx.space/restic";
+      timerConfig = {
+        OnCalendar = "Mon,Wed,Sat";
+        Persistent = true;
+      };
+    };
   };
 
   users.users.stommydx.extraGroups = [ "libvirtd" "vboxusers" ];
