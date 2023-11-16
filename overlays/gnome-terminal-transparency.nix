670e63987bce1ab6696a39bf015846ef9337f907
Tommy Li <dev@stdx.space>
2023-11-16 01:42:44 -0500

    chore: bump gnome terminal version

--- overlays/gnome-terminal-transparency.nix
+++ overlays/gnome-terminal-transparency.nix
@@ -7,19 +7,19 @@ self: super:
   gnome = super.gnome.overrideScope' (gself: gsuper: {
     gnome-terminal = gsuper.gnome-terminal.overrideAttrs (old: rec {
       # downgrade to support AUR patch
-      version = "3.48.1";
+      version = "3.48.2";
       src = super.fetchFromGitLab {
         domain = "gitlab.gnome.org";
         owner = "GNOME";
         repo = "gnome-terminal";
         rev = version;
-        sha256 = "sha256-1t48JRESjAQubOmyK+QOhlp57iE5Ml0cqgy/2wjrLjE=";
+        sha256 = "sha256-WvFKFh5BK6AS+Lqyh27xIfH1rxs1+YTkywX4w9UashQ=";
       };
 
       patches = [
         (super.fetchpatch {
-          url = "https://aur.archlinux.org/cgit/aur.git/plain/transparency.patch?h=gnome-terminal-transparency&id=abddafca02e4a353db2780b0d3c3eceaedaccebc";
-          sha256 = "sha256-noCHCoM9e/ZuoFQlSDUEXSNk9irYPS0MeyVpVJQOQWk=";
+          url = "https://aur.archlinux.org/cgit/aur.git/plain/transparency.patch?h=gnome-terminal-transparency&id=fcfc3f2ccf9cdc0a55e07bafc6efc25310bdf4c3";
+          sha256 = "sha256-tnzhEyqTpd6AHVACf4Sgwe4yYLWZ+mWPmlLXGMsqzBY=";
         })
       ];
     });
