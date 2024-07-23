7d06de858f38d1df8a8b749ce2e77f0dfb3319a6
Tommy Li <dev@stdx.space>
2024-07-24 00:11:51 +0800

    disable network manager for wsl to manage network

--- config/wsl/hosts/winpcdx/configuration.nix
+++ config/wsl/hosts/winpcdx/configuration.nix
@@ -6,4 +6,7 @@
   ];
 
   networking.hostName = "winpcdx";
+
+  # disable network manager for wsl to manage interfaces for mirrored netword mode
+  networking.networkmanager.enable = lib.mkForce false;
 }
