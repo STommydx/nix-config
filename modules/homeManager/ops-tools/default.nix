d8fdf41a6af3ce0998d0d8798acaf30b31ef1b4c
Tommy Li <dev@stdx.space>
2025-01-16 20:39:04 +0800

    add nix index to ops tools

--- modules/homeManager/ops-tools/default.nix
+++ modules/homeManager/ops-tools/default.nix
@@ -1,5 +1,12 @@
-{ ... }:
+{ inputs, ... }:
 
 {
+
+  imports = [
+    inputs.nix-index-database.hmModules.nix-index
+  ];
+
+  # use precompiled nix-index database
+  programs.nix-index-database.comma.enable = true;
 
 }
