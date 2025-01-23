1ba165e2ba2ac2564ff4f81dfc32413907513f04
Tommy Li <dev@stdx.space>
2025-01-24 01:08:11 +0800

    chore: update nix flake inputs

--- profiles/nixos/alltheway-desktop/default.nix
+++ profiles/nixos/alltheway-desktop/default.nix
@@ -8,7 +8,7 @@
     ../../../modules/nixos/dev-tools
     ../../../modules/nixos/dev-tools-desktop
     ../../../modules/nixos/gaming
-    ../../../modules/nixos/machine-learning
+    # ../../../modules/nixos/machine-learning # RIP: https://github.com/NixOS/nixpkgs/issues/375745
     ../../../modules/nixos/mobile-development
     ../../../modules/nixos/multimedia
     ../../../modules/nixos/office-tools
