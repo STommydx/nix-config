2fea3fe900a3bd25bb775c4c1ad488e3e66467c5
Tommy Li <dev@stdx.space>
2025-02-04 23:48:25 +0800

    edit home path at makcpu1

--- hosts/homeManager/makcpu1/default.nix
+++ hosts/homeManager/makcpu1/default.nix
@@ -5,6 +5,6 @@
   imports = [ ../../../profiles/homeManager/devops ];
 
   home.username = lib.mkForce "thomasli";
-  home.homeDirectory = lib.mkForce "/home/thomasli";
+  home.homeDirectory = lib.mkForce "/homes/thomasli";
 
 }
