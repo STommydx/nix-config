38394a6587c578c1d0e8b40d40dd8f7dec2e0db8
Tommy Li <dev@stdx.space>
2025-02-12 11:32:16 +0800

    add conda init hook to makcpu1 zsh

--- hosts/homeManager/makcpu1/default.nix
+++ hosts/homeManager/makcpu1/default.nix
@@ -7,6 +7,10 @@
   home.username = lib.mkForce "thomasli";
   home.homeDirectory = lib.mkForce "/homes/thomasli";
 
+  programs.zsh.initExtra = ''
+    eval "$(/2tssd/thomasli/miniconda3/bin/conda shell.bash hook)"
+  '';
+
   xdg.cacheHome = lib.mkForce "/2tssd/thomasli/home/.cache";
   xdg.dataHome = lib.mkForce "/2tssd/thomasli/home/.local/share";
   xdg.stateHome = lib.mkForce "/2tssd/thomasli/home/.local/state";
