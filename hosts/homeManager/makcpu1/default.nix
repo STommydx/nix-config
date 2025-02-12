978b3696a09f06d0b2d0f1259ee51b787f7a5e09
Tommy Li <dev@stdx.space>
2025-02-12 11:11:58 +0800

    update xdg home locations for makcpu1

--- hosts/homeManager/makcpu1/default.nix
+++ hosts/homeManager/makcpu1/default.nix
@@ -7,4 +7,8 @@
   home.username = lib.mkForce "thomasli";
   home.homeDirectory = lib.mkForce "/homes/thomasli";
 
+  xdg.cacheHome = lib.mkForce "/2tssd/thomasli/home/.cache";
+  xdg.dataHome = lib.mkForce "/2tssd/thomasli/home/.local/share";
+  xdg.stateHome = lib.mkForce "/2tssd/thomasli/home/.local/state";
+
 }
