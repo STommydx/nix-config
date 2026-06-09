4d876543a6be9da18fbb730075da4fd822e7ec76
Tommy Li <dev@stdx.space>
2026-06-09 16:35:14 +0800

    refactor(nixos): remove ollama and add lmstudio
    Remove the commented-out ollama service configuration from the machine-learning module. Add lmstudio to system packages as the preferred local LLM GUI option.

--- modules/nixos/machine-learning/default.nix
+++ modules/nixos/machine-learning/default.nix
@@ -5,15 +5,8 @@
   environment.systemPackages = with pkgs; [
     amdgpu_top
     rocmPackages.rocminfo
+    lmstudio
   ];
-
-  # Temporarily disabled due to hipblaslt build failure
-  # https://github.com/NixOS/nixpkgs/issues/449880
-  # services.ollama = {
-  #   enable = true;
-  #   acceleration = "rocm";
-  #   rocmOverrideGfx = "11.0.0"; # TODO: make this configurable via options
-  # };
 
   # Add rule for using rocm in deep learning libraries
   # https://nixos.wiki/wiki/AMD_GPU
